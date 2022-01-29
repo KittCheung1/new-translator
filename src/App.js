2f96176beede13bc93bfa38d0e4a1efdd52e2099
Kitt Cheung <kitt.cheung@live.se>
2022-01-29 12:59:16 +0100

    add files from old repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from "./components/Login"
+import Translator from "./components/Translator";
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import UserProvider from "./components/Context/UserContext"
+import WordsProvider from "./components/Context/WordsContext"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+      <WordsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/translator" element={<Translator />}></Route>
+        </Routes>
+      </BrowserRouter>
+      </WordsProvider>
+    </UserProvider>
   );
 }
 
